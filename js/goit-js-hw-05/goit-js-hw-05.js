181ba26429bd164b6e2d5840b992cdf0d5763596
Vovanchuk04071 <57066225+Vovanchuk04071@users.noreply.github.com>
2020-12-08 02:34:36 +0200

    added new home-work

--- js/goit-js-hw-05/goit-js-hw-05.js
+++ js/goit-js-hw-05/goit-js-hw-05.js
@@ -128,23 +128,120 @@
 
 
 // ========= Завдання №4
-class StringBuilder {
-    constructor(value) {
-        this._value = value;
-    }
-    get value() {
-        return this.value
-    }
- }
+// class StringBuilder {
+//     constructor(value) {
+//         this._value = value;
+//     }
+//     get value() {
+//         return this._value;
+//     }  
 
-const builder = new StringBuilder('.')
+//     append (str) {
+//         this._value += str
+//     };
 
 
-builder.append('^');
-console.log(builder.value); // '.^'
+//     prepend(str) {
+//     this._value = str + this._value;
+//     }
 
-builder.prepend('^');
-console.log(builder.value); // '^.^'
+//     pad(str) {
+//         this._value = str + this._value + str;
+//     }
+//  }
 
-builder.pad('=');
-console.log(builder.value); // '=^.^='
+// const builder = new StringBuilder('.')
+
+// console.log(builder)
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
+
+// ==================== Завдання №5
+
+// class Car {
+//     static getSpecs(car) {
+//         console.log(car.maxSpeed)
+//         console.log(car.speed)
+//         console.log(car.isOn)
+//         console.log(car.distance)
+//         console.log(car.price)
+//     }  
+
+//     constructor({ speed = 0, price, maxSpeed, isOn = false, distance = 0 }) {
+//         this.speed = speed;
+//         this.price = price;
+//         this.maxSpeed = maxSpeed;
+//         this.isOn = isOn;
+//         this.distance = distance;
+//     }
+
+//     get _price () {
+//         return this.price
+//     }
+
+//     set _price(value) {
+//         this.price = value;
+//     }
+
+//     turnOn() {
+//         this.isOn = true;
+        
+//     }
+
+//     turnOff() {
+//         this.isOn = false;
+//         this.speed = 0;
+        
+//     }
+//     accelerate(value) {
+//         if (this.speed + value < this.maxSpeed) {
+//             this.speed = this.speed + value
+//         }
+//     }
+
+//     decelerate(value) {
+//         if (this.speed - value > 0) {
+//             this.speed = this.speed - value;
+//         }        
+//     }
+
+//     drive(hours) {
+//         if (this.turnOn = true) {
+//             this.distance = this.distance + hours * this.speed
+//         }
+//     }
+    
+// }
+
+
+// const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+// mustang.turnOn();
+// mustang.accelerate(50);
+// mustang.drive(2);
+
+
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+// mustang.decelerate(20);
+// mustang.drive(1);
+// mustang.turnOff();
+
+// Car.getSpecs(mustang);
+// // maxSpeed: 200, speed: 30, isOn: false, distance: 130, price: 2000
+
+// console.log(mustang.price); // 2000
+// mustang.price = 4000;
+// console.log(mustang.price); // 4000
+
+
+// // console.log(mustang.speed)
