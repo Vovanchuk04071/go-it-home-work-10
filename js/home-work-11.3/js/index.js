644325ec88ee261ca3e4d9899d52f51aa37790b8
Vovanchuk04071 <57066225+Vovanchuk04071@users.noreply.github.com>
2021-01-05 22:46:43 +0200

    change work

--- js/home-work-11.3/js/index.js
+++ js/home-work-11.3/js/index.js
@@ -1,43 +1,59 @@
 const refs = {
-    day: document.querySelector('[data-value="days"]'),
-    hours: document.querySelector('[data-value="hours"]'),
-    mins: document.querySelector('[data-value="mins"]'),
-    secs: document.querySelector('[data-value="secs"]'),
-}
+  timer: document.querySelector("#timer-1"),
+  day: document.querySelector('[data-value="days"]'),
+  hours: document.querySelector('[data-value="hours"]'),
+  mins: document.querySelector('[data-value="mins"]'),
+  secs: document.querySelector('[data-value="secs"]'),
+};
 
 class CountdownTimer {
-    constructor ({selector, targetDate}) {
-        this.selector = selector;
-        this.targetDate = targetDate;
-    }
+  constructor({ selector, targetDate }) {
+    this.selector = selector;
+    this.targetDate = targetDate;
+  }
+  // createHtmlTimer () {
+  //     const time = document.querySelector(".timer")
+  //     time.setAttribute('id', this.selector)
+  //     for (let i = 1; 4 >= i; i += 1) {
+  //         let marcup = document.createElement('div')
+  //         marcup.classList.add('field')
+  //         const elem = document.createElement('span')
+  //         elem.setAttribute
+  //         time.prepend(marcup)
+  //         marcup.prepend(elem)
+
+  //     }
+
+  //     // time.innerHTML = marcup;
+  //     console.log(time)
+  // };
 }
 
 const timer = new CountdownTimer({
-    selector: '#timer-1',
-    targetDate: new Date('Jan 5, 2021'),
+  selector: "#timer-2",
+  targetDate: new Date("Jan 27, 2021"),
 });
 
-function differentTime() {
-    const today = Date.now();
-    const futureTime = timer.targetDate.getTime();
-    return futureTime - today;
-}
+// console.log(timer.createHtmlTimer())
 
+function differentTime() {
+  const today = Date.now();
+  const futureTime = timer.targetDate.getTime();
+  return futureTime - today;
+}
 
 const timerId = setInterval(() => {
-    const time = differentTime()
-    const days = Math.floor(time / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((time % (1000 * 60)) / 1000);
-    updateClockFace({days, hours, mins,  secs});
-}, 1000)
+  const time = differentTime();
+  const days = Math.floor(time / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((time % (1000 * 60)) / 1000);
+  updateClockFace({ days, hours, mins, secs });
+}, 1000);
 
-
-function updateClockFace({days, hours, mins,  secs}) {
-    refs.day.textContent = days;
-    refs.hours.textContent = hours;
-    refs.mins.textContent = mins;
-    refs.secs.textContent = secs;
+function updateClockFace({ days, hours, mins, secs }) {
+  refs.day.textContent = days;
+  refs.hours.textContent = hours;
+  refs.mins.textContent = mins;
+  refs.secs.textContent = secs;
 }
-
